c72c7849a1efa9e6b4007aeaa9c41790d799ed77
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-02-09 17:34:25 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ The example module supplies one function, factorial().  For example,
 >>> factorial(5)
 120
 """
- 
+
 
 def factorial(n):
     """Return the factorial of n, an exact integer >= 0.
@@ -36,7 +36,7 @@ def factorial(n):
     """
 
     import math
-     
+
     if not n >= 0:
         raise ValueError("n must be >= 0")
     if math.floor(n) != n:
