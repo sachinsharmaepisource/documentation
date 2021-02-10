646bb0d15512440ab618deba732d62b77f7904c3
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-02-10 10:57:02 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,3 @@
-"""
-This is the "example" module.
-
-The example module supplies one function, factorial().  For example,
-
->>> factorial(5)
-120
-"""
-
-
 def factorial(n):
     """Return the factorial of n, an exact integer >= 0.
 
