8367ec756ba7149c8e6b5bb605371bd1bb7675fe
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-02-10 17:12:09 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -10,7 +10,29 @@ The example module supplies one function, factorial().  For example,
 
 def factorial(n):
     """Return the factorial of n, an exact integer >= 0.
-       Random
+
+    >>> [factorial(n) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+    >>> factorial(30)
+    265252859812191058636308480000000
+    >>> factorial(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be >= 0
+
+    Factorials of floats are OK, but the float must be an exact integer:
+    >>> factorial(30.1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be exact integer
+    >>> factorial(30.0)
+    265252859812191058636308480000000
+
+    It must also not be ridiculously large:
+    >>> factorial(1e100)
+    Traceback (most recent call last):
+        ...
+    OverflowError: n too large
     """
     import math
 
