7094b01267f3229c2fbcb0d92d2011e7a2942a29
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-02-12 12:59:34 +0530

    Update test1.py

--- src/test1.py
+++ src/test1.py
@@ -30,9 +30,7 @@ def factorial(n_n, m_m):
         Array_like means all those objects -- lists, nested lists, etc. --
         that can be converted to an array.  We can also refer to
         variables like `var1`.
-        
-    Args:
-        m_m (float): bla m
+
 
     >>> [factorial(n) for n in range(6)]
     [1, 1, 2, 6, 24, 120]
