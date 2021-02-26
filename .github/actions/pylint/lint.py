49c209fa4362172f74f655053a14ac87e5f129f8
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-02-27 00:29:32 +0530

    Update lint.py

--- .github/actions/pylint/lint.py
+++ .github/actions/pylint/lint.py
@@ -83,7 +83,7 @@ class PyLintComuptation:
                  'Threshold: {} |'.format(path, threshold))
 
 #     results = Run([path], do_exit=False)
-    path = ''
+    path = '.'
     pylint_opts = [ path, '--rcfile=./.pylintrc']
     results = Run(pylint_opts, do_exit=False)
     
