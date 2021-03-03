16a546dad2d422d6bccab022ddc03b2e35321845
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-03-03 16:50:34 +0530

    Update main.py

--- .github/actions/check-docstrings/main.py
+++ .github/actions/check-docstrings/main.py
@@ -86,7 +86,7 @@ class CheckDocstrings:
     commit = self.branch.commit
     return commit.sha
 
-  def create_review_comments(self, user_name, pull_number, body, file_path, position):
+  def post_create_review_comment(self, user_name, pull_number, body, file_path, position):
     '''
       Parameters
       ----------
@@ -113,7 +113,30 @@ class CheckDocstrings:
     }
     r = requests.post(query_url, headers=self.header, data=json.dumps(data))
     # pprint(r.json())
-    
+  
+  def create_review_comments(self, report_dct_):
+    '''
+      Parameters
+      ----------
+          report_dct_: string
+      Logic
+      ----------
+          Then it will create new review comments.
+      Return
+      ----------
+          None
+    '''
+    for report_section in self.report_dct_in_pr_rev_cmnt:
+      for lst in report_dct_[report_section]:
+        print(lst)
+        path = lst[0]
+        desc_ = lst[1]
+        desc_ = f'{self.LABEL} \n {desc_}'
+        splt = path.split(':', 2)
+        file_path = splt[0]
+        line_no_ = int(splt[1])
+        self.post_create_review_comment(self.USER_NAME, self.PR_NUMBER, desc_, file_path, line_no_)
+
   def check_docstrings(self, file_paths):
     '''
       Parameters
@@ -122,7 +145,7 @@ class CheckDocstrings:
           
       Logic
       ----------
-          execute lint on all the files and then create review comment on the current pull request.
+          execute lint on all the files and then call create review comment on the current pull request.
       Return
       ----------
           None
@@ -141,17 +164,7 @@ class CheckDocstrings:
         desc_ = splt[2] if len(splt) >= 3 else 'DEFAULT_DESC'
         if type_ in report_dct_.keys():
           report_dct_[type_].append([path_, desc_])
-      
-      for report_section in self.report_dct_in_pr_rev_cmnt:
-        for lst in report_dct_[report_section]:
-          print(lst)
-          path = lst[0]
-          desc_ = lst[1]
-          desc_ = f'{self.LABEL} \n {desc_}'
-          splt = path.split(':', 2)
-          file_path = splt[0]
-          line_no_ = int(splt[1])
-          self.create_review_comments(self.USER_NAME, self.PR_NUMBER, desc_, file_path, line_no_)
+      self.create_review_comments(report_dct_)
 
   def compute(self):
     '''
