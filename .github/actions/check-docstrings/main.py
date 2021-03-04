35c834f26cc00690e6ba843a17283f8ae18dffbd
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-03-04 08:56:30 +0530

    Update main.py

--- .github/actions/check-docstrings/main.py
+++ .github/actions/check-docstrings/main.py
@@ -2,11 +2,11 @@ import os
 import os.path
 import sys
 from github import Github
- 
+
 from pylint import epylint as lint
 import json
 import requests
-from pprint import pprint 
+from pprint import pprint
 #---------------------------------------------------------------------------------------------------------
 '''
 	Logic:
@@ -34,6 +34,7 @@ class CheckDocstrings:
     self.report_dct = { 'errors': [], 'convention': [], 'refactor': [], 'warning': [] }
     self.report_dct_in_pr_rev_cmnt = { 'convention': [] }
     self.LABEL = '[CHECK DOCSTRINGS]'
+    self.help_link = 'https://numpydoc.readthedocs.io/en/latest/example.html'
 
     
   def get_inputs(self, input_name):
@@ -86,7 +87,7 @@ class CheckDocstrings:
     commit = self.branch.commit
     return commit.sha
 
-  def create_review_comments(self, user_name, pull_number, body, file_path, position):
+  def post_create_review_comment(self, user_name, pull_number, body, file_path, position):
     '''
       Parameters
       ----------
@@ -103,7 +104,6 @@ class CheckDocstrings:
       ----------
           None
     '''
-    self.delete_all_previous_bot_generated_review_comments(pull_number)
     query_url = f"https://api.github.com/repos/{user_name}/pulls/{pull_number}/comments"
     data = {
         "body": body,
@@ -113,7 +113,70 @@ class CheckDocstrings:
     }
     r = requests.post(query_url, headers=self.header, data=json.dumps(data))
     # pprint(r.json())
-    
+  
+  def create_review_comments(self, report_dct_):
+    '''
+      Parameters
+      ----------
+          report_dct_: string
+      Logic
+      ----------
+          It will create new review comments.
+      Return
+      ----------
+          None
+    '''
+    for report_section in self.report_dct_in_pr_rev_cmnt:
+      for lst in report_dct_[report_section]:
+        print(lst)
+        path = lst[0]
+        desc_ = lst[1]
+        desc_ = f'{self.LABEL} \n {desc_} \n {self.help_link}'
+        splt = path.split(':', 2)
+        file_path = splt[0]
+        line_no_ = int(splt[1])
+        self.post_create_review_comment(self.USER_NAME, self.PR_NUMBER, desc_, file_path, line_no_)
+
+  def get_params_from_pylint_stdout(self, splt):
+    '''
+      Parameters
+      ----------
+          splt: list
+      Logic
+      ----------
+          Format the pylint stdout splt
+      Return
+      ----------
+          None
+    '''
+    if splt[0]=='':
+      splt.pop(0)
+    path_ = splt[0]
+    type_ = splt[1] if len(splt) >= 2 else 'DEFAULT_TYPE'
+    desc_ = splt[2] if len(splt) >= 3 else 'DEFAULT_DESC'
+    return path_, type_, desc_
+
+  def format_pylint_stdout(self, report_dct_, pylint_stdout):
+    '''
+      Parameters
+      ----------
+          report_dct_: string
+          pylint_stdout: string
+      Logic
+      ----------
+          Format the pylint stdout stream
+      Return
+      ----------
+          None
+    '''
+    for line in pylint_stdout:  # Iterate through the cStringIO file-like object.
+      line.strip()
+      splt = line.split(' ', 3)
+      path_, type_, desc_ = self.get_params_from_pylint_stdout(splt)
+      if type_ in report_dct_.keys():
+        report_dct_[type_].append([path_, desc_])
+    self.create_review_comments(report_dct_)
+
   def check_docstrings(self, file_paths):
     '''
       Parameters
@@ -122,7 +185,7 @@ class CheckDocstrings:
           
       Logic
       ----------
-          execute lint on all the files and then create review comment on the current pull request.
+          execute lint on all the files and then call create review comment on the current pull request.
       Return
       ----------
           None
@@ -131,27 +194,7 @@ class CheckDocstrings:
       (pylint_stdout, pylint_stderr) = lint.py_run(file_path, return_std=True)
       pylint_stdout.seek(0)
       report_dct_ = self.report_dct
-      for line in pylint_stdout:  # Iterate through the cStringIO file-like object.
-        line.strip()
-        splt = line.split(' ', 3)
-        if splt[0]=='':
-          splt.pop(0)
-        path_ = splt[0]
-        type_ = splt[1] if len(splt) >= 2 else 'DEFAULT_TYPE'
-        desc_ = splt[2] if len(splt) >= 3 else 'DEFAULT_DESC'
-        if type_ in report_dct_.keys():
-          report_dct_[type_].append([path_, desc_])
-      
-      for report_section in self.report_dct_in_pr_rev_cmnt:
-        for lst in report_dct_[report_section]:
-          print(lst)
-          path = lst[0]
-          desc_ = lst[1]
-          desc_ = f'{self.LABEL} \n {desc_}'
-          splt = path.split(':', 2)
-          file_path = splt[0]
-          line_no_ = int(splt[1])
-          self.create_review_comments(self.USER_NAME, self.PR_NUMBER, desc_, file_path, line_no_)
+      self.format_pylint_stdout(report_dct_, pylint_stdout)
 
   def compute(self):
     '''
@@ -165,6 +208,8 @@ class CheckDocstrings:
       ----------
           None
     '''
+    pull_number = self.PR_NUMBER
+    self.delete_all_previous_bot_generated_review_comments(pull_number)
     contents = self.repo.get_contents("", self.branch.name)
     file_paths = []
     while contents:
